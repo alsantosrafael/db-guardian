8d46becc9f840110a03df965b12cc1c255b6e22f
Rafael Almeida <rafael.profeng@gmail.com>
2025-07-29 16:43:46 -0300

    feat: Improve S3 provision and infra job

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,14 +7,106 @@ terraform {
   }
 }
 # Provider
-
-
 provider "aws" {
   region = "us-east-1"
 }
 
-# bucket
+# Data Source - Retrieving user info
+data "aws_caller_identity" "current" {}
 
+# Bucket definition
 resource "aws_s3_bucket" "reports" {
   bucket = "query-analyzer-reports"
+}
+
+# Cryptography
+resource "aws_s3_bucket_server_side_encryption_configuration" "reports" {
+  bucket = aws_s3_bucket.reports.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+# Blocking public access
+resource "aws_s3_bucket_public_access_block" "reports" {
+  bucket = aws_s3_bucket.reports.id
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+# Automatic backup
+resource "aws_s3_bucket_versioning" "reports" {
+  bucket = aws_s3_bucket.reports.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Automatic removal of files
+resource "aws_s3_bucket_lifecycle_configuration" "reports" {
+  rule {
+    id     = "auto_cleanup"
+    status = "Enabled"
+
+    expiration {
+      days = 30
+    }
+  }
+}
+
+
+# Protected URL only
+resource "aws_s3_bucket_policy" "reports" {
+  bucket = aws_s3_bucket.reports.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowApplicationAccess"
+        Effect    = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/query-analyzer-role"
+        }
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "${aws_s3_bucket.reports.arn}/*"
+      },
+      {
+        Sid       = "DenyDirectPublicAccess"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.reports.arn}/*"
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      },
+      {
+        Sid       = "RequireSSLRequestsOnly"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          aws_s3_bucket.reports.arn,
+          "${aws_s3_bucket.reports.arn}/*"
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      }
+    ]
+  })
+  depends_on = [aws_s3_bucket_public_access_block.reports]
 }
